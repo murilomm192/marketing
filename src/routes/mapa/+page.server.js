387342739190a8acff2bb2959c677b738625a1ae
Murilo Martins <murilomm192@gmail.com>
2024-09-04 23:46:44 -0300

    rewriten query to use coalesce

--- src/routes/mapa/+page.server.js
+++ src/routes/mapa/+page.server.js
@@ -7,6 +7,19 @@ import { db, supabase } from '$lib/db';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+/**
+ * Retrieves public URLs for a specific column in the given dataset.
+ *
+ * @param {Array} base - The original dataset to process.
+ * @param {string} bucket - The name of the storage bucket.
+ * @param {string} column - The name of the column containing file paths.
+ * @returns {Array} A new array with the original data and updated public URLs.
+ *
+ * This function takes a dataset, a storage bucket name, and a column name.
+ * It then creates a new array where each row of the original dataset is updated
+ * with a public URL for the file specified in the given column.
+ * The public URL is generated using Supabase's storage.from().getPublicUrl() method.
+ */
 function get_public_url(base, bucket, column) {
   const baseurl = base.map(row => ({ ...row, [column]: supabase.storage.from(bucket).getPublicUrl(row[column]).data.publicUrl }))
   return baseurl
@@ -25,28 +38,36 @@ select
   v.chave,
   v.ano,
   v.mes,
-  case when v.cerveja is null then 0 else v.cerveja end,
- case when v.nab is null then 0 else v.nab end,
- case when v.rgb is null then 0 else v.rgb end,
- case when v.brahma is null then 0 else v.brahma end,
- case when v.spaten is null then 0 else v.spaten end,
- case when v.corona is null then 0 else v.corona end,
- case when v.bud is null then 0 else v.bud end,
- case when v.stella is null then 0 else v.stella end,
- case when v.skol is null then 0 else v.skol end,
- case when v.antarctica is null then 0 else v.antarctica end,
- case when v.guarana is null then 0 else v.guarana end,
-  case when v.beats is null then 0 else v.beats end
-from volume as v right join coleta_pdv as c on c.chave = v.chave
+  coalesce(v.cerveja, 0) as cerveja,
+  coalesce(v.nab, 0) as nab,
+  coalesce(v.rgb, 0) as rgb,
+  coalesce(v.brahma, 0) as brahma,
+  coalesce(v.spaten, 0) as spaten,
+  coalesce(v.corona, 0) as corona,
+  coalesce(v.bud, 0) as bud,
+  coalesce(v.stella, 0) as stella,
+  coalesce(v.skol, 0) as skol,
+  coalesce(v.antarctica, 0) as antarctica,
+  coalesce(v.guarana, 0) as guarana,
+  coalesce(v.beats, 0) as beats
+from coleta_pdv as c
+left join volume as v on c.chave = v.chave
 `)
 
-  base = get_public_url(base, 'Fachadas', 'fachada')
-  base = get_public_url(base, 'Cardapios', 'cardapio')
-  base = get_public_url(base, 'Interiores', 'interior')
+const buckets = ['Fachadas', 'Cardapios', 'Interiores'];
+const columns = ['fachada', 'cardapio', 'interior'];
 
-  return {
-    ok: 'ok',
-    result: base,
-    volumes: volumes
-  }
+base = base.map(row => {
+  buckets.forEach((bucket, index) => {
+    const column = columns[index];
+    row[column] = supabase.storage.from(bucket).getPublicUrl(row[column]).data.publicUrl;
+  });
+  return row;
+});
+
+return {
+  ok: 'ok',
+  result: base,
+  volumes
+};
 })
